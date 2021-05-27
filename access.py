739d543d2a7326483ac758441c038f3d90a5c126
keerthipriyaulchi <84890884+keerthipriyaulchi@users.noreply.github.com>
2021-05-27 09:46:09 +0530

    Add files via upload

---
+++ access.py
@@ -0,0 +1,6 @@
+mylist=[212.56,-3,"Book",["Java","Python","c"],"a12"]
+for x in range(0,5):
+    print("The",x+1,"element is:",mylist[x])
+print("The first item of nested list is:",mylist[3][0])
+print("The second item of nested list is:",mylist[3][1])
+print("The third item of nested list is:",mylist[3][2])
